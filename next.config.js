59330bfa576c920f299361b9c26d435d35704b9c
haeseungsung <haeseungsung@users.noreply.github.com>
2026-01-21 01:39:01 +0900

    Add outputFileTracingRoot to fix Vercel symlink issue

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
+const path = require('path');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  experimental: {
+    outputFileTracingRoot: path.join(__dirname, '../../'),
+  },
 };
 
 module.exports = nextConfig;
